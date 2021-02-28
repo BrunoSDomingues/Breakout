e40ec48c49db80127c3bb0be3e8f01346f45630b
Bruno Domingues <bruno_signorelli@outlook.com>
2021-02-27 23:36:58 -0300

    Start of project + some codes for movment

---
+++ Assets/_Scripts/BallMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+
+    [Range(1, 15)]
+    public float ballSpeed = 5.0f;
+
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        float dirX = Random.Range(-5.0f, 5.0f);
+        float dirY = Random.Range(1.0f, 5.0f);
+
+        direction = new Vector3(dirX, dirY).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * ballSpeed);
+
+        Vector2 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (viewportPosition.x < 0 || viewportPosition.x > 1)
+        {
+            direction = new Vector3(-direction.x, direction.y);
+        }
+        if (viewportPosition.y < 0 || viewportPosition.y > 1)
+        {
+            direction = new Vector3(direction.x, -direction.y);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/PaddleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public float paddleSpeed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(inputX, 0, 0) * Time.deltaTime * paddleSpeed);
+    }
+}
